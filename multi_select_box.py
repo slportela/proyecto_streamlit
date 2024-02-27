d44cff59e3ec44688e677236c99e55fd0867f27f
Sebastian Portela <sebastian@marvik.ai>
2024-02-27 02:18:37 -0300

    update

---
+++ multi_select_box.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'], help="just pick your fav colors...")
+
+st.write('You selected:', options)
